ad4f27df08bf6929ba21155f3ad40bc4558307f8
Matthew <863968+mattbis@users.noreply.github.com>
2023-04-02 15:38:33 +0100

    Update mapFilePath.rb

--- lib/0/mapFilePath.rb
+++ lib/0/mapFilePath.rb
@@ -1,1 +1,38 @@
+require 'find'
+require 'json'
 
+def map_file_path_recursive(path)
+  results = []
+
+  Find.find(path) do |file|
+    next if File.directory?(file)
+
+    begin
+      stat = File.stat(file)
+      result = {
+        path: file,
+        stats: {
+          mode: stat.mode,
+          ino: stat.ino,
+          dev: stat.dev,
+          nlink: stat.nlink,
+          uid: stat.uid,
+          gid: stat.gid,
+          rdev: stat.rdev,
+          size: stat.size,
+          blksize: stat.blksize,
+          blocks: stat.blocks,
+          atime: stat.atime,
+          mtime: stat.mtime,
+          ctime: stat.ctime,
+          birthtime: stat.birthtime
+        }
+      }
+      results << result
+    rescue
+      results << { path: file, error: $!.to_s }
+    end
+  end
+
+  JSON.pretty_generate(results)
+end
